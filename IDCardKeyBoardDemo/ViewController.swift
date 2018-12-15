7bc96662bc321fb4eaf0e545b8218b621dadf039
lidongxi <863223764@qq.com>
2018-12-15 19:44:18 +0800

    b

--- IDCardKeyBoardDemo/ViewController.swift
+++ IDCardKeyBoardDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let id_cardTextField = DXTextField.init(frame: CGRect.init(x: 20, y: 200, width: 200, height: 40))
+        let id_cardTextField = DXTextField.init(frame: CGRect.init(x: 20, y: 50, width: 200, height: 40))
         id_cardTextField.borderStyle = .roundedRect
         id_cardTextField.placeholder = "code:请输入身份证号码"
         view.addSubview(id_cardTextField)
